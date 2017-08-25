8a267e1eafd84d26604ab4c5d83a558a9ee01375
James Lang <hi@thejameslang.com>
2017-08-25 17:03:03 -0500

    make prototype root

--- src/router/index.js
+++ src/router/index.js
@@ -8,12 +8,12 @@ Vue.use(Router);
 export default new Router({
   routes: [
     {
-      path: '/',
+      path: '/hello',
       name: 'Hello',
       component: Hello,
     },
     {
-      path: '/offers',
+      path: '/',
       name: 'Offers',
       component: Offers,
     },
